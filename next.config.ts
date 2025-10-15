6dbd94037fbb7ba91b6ecaac3acef09af8289f68
Krzysztof Piasecki <krzysztof.piasecki@capgemini.com>
2025-10-15 15:17:40 +0200

    Preloading

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-    // experimental: {
-    //     ppr: 'incremental'
-    // }
+    experimental: {
+        cacheComponents: true,
+    }
 };
 
 export default nextConfig;
